3303ee54ec749160d053de388694bdcd6f9bf238
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2023-05-10 15:48:02 -0300

    Database seed updated

--- src/main/java/com/nxstage/nxgamelist/domain/model/GameList.java
+++ src/main/java/com/nxstage/nxgamelist/domain/model/GameList.java
@@ -1,8 +1,6 @@
 package com.nxstage.nxgamelist.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -15,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class GameList {
     @Id
     @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+    private String name;
 }
